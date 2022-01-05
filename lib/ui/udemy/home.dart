e79462b19a02645ebe9c3ff111bc648931efa63f
nopezi saputra <snopezi@gmail.com>
2022-01-05 23:39:11 +0700

    udemy body menu icon

--- lib/ui/udemy/home.dart
+++ lib/ui/udemy/home.dart
@@ -72,11 +72,102 @@ class Home extends StatelessWidget {
     ),
   );
 
+  final bodyRow = Container(
+    color: Colors.green,
+    child: Row(
+      // mainAxisSize: MainAxisSize.min,
+      // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      // crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        // Column(),
+        Icon(
+          Icons.headset,
+          size: 50,
+        ),
+        Icon(
+          Icons.headset,
+          size: 50,
+        ),
+        Icon(
+          Icons.headset,
+          size: 50,
+        ),
+        Icon(
+          Icons.headset,
+          size: 50,
+        ),
+      ],
+    ),
+  );
+
+  final bodyMenuIcon = Center(
+    child: Container(
+        color: Colors.grey[200],
+        height: 80,
+        width: double.infinity,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.phone,
+                  size: 30,
+                  color: Colors.blue,
+                ),
+                SizedBox(
+                  height: 8,
+                ),
+                Text(
+                  "CALL",
+                  style: TextStyle(color: Colors.blue),
+                ),
+              ],
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.navigation,
+                  size: 30,
+                  color: Colors.blue,
+                ),
+                SizedBox(
+                  height: 8,
+                ),
+                Text(
+                  "ROUTE",
+                  style: TextStyle(color: Colors.blue),
+                )
+              ],
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.share,
+                  size: 30,
+                  color: Colors.blue,
+                ),
+                SizedBox(
+                  height: 8,
+                ),
+                Text(
+                  "SHARE",
+                  style: TextStyle(color: Colors.blue),
+                )
+              ],
+            ),
+          ],
+        )),
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: appbar,
-      body: bodyKolom,
+      body: bodyMenuIcon,
     );
   }
 }
